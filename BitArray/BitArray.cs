4ced9ea7c0316944cc256118dd6ebd0892fc2588
Nick Angelov <nangelov1990@gmail.com>
2015-01-29 11:45:41 +0200

    BitArray Task Completed

---
+++ BitArray/BitArray.cs
@@ -0,0 +1,106 @@
+﻿namespace BitArray
+{
+    using System;
+    using System.Numerics;
+
+    public class BitArray
+    {
+        private readonly uint _bitsCount;
+        private string[] _bitValues;
+        private const int MaxSize = 100000;
+
+        public BitArray(uint bitCount)
+        {
+            this._bitsCount = bitCount;
+            CreateBitArray(BitsCount);
+        }
+
+        private void CreateBitArray(uint bitCount)
+        {
+            if (bitCount <= MaxSize)
+            {
+                BitValues = new string[bitCount];
+
+                for (uint i = 0; i < bitCount; i++)
+                {
+                    this.BitValues[i] = "0";
+                }
+            }
+            else
+            {
+                throw new IndexOutOfRangeException(String.Format(
+                        "Size {0} is invalid!",
+                        bitCount));
+            }
+        }
+
+        private uint BitsCount
+        {
+            get { return this._bitsCount; }
+        }
+
+        private string[] BitValues
+        {
+            get { return this._bitValues; }
+            set { this._bitValues = value; }
+        }
+
+        // Indexer declaration
+        public byte this[uint index]
+        {
+            get
+            {
+                if (index < BitsCount)
+                {
+                    return byte.Parse(this.BitValues[index]);
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Index {0} is invalid!",
+                        index));
+                }
+
+                
+            }
+
+            set
+            {
+                if (index > BitsCount - 1)
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Index {0} is invalid!",
+                        index));
+                }
+                if (value > 1)
+                {
+                    throw new ArgumentException(String.Format(
+                        "Value {0} is invalid!",
+                        value));
+                }
+
+                this.BitValues.SetValue(value.ToString(), index);
+            }
+        }
+
+        private BigInteger CalculateValue()
+        {
+            BigInteger output = 0;
+
+            for (int i = 0; i < this._bitsCount; i++)
+            {
+                if (BitValues[i] == "1")
+                {
+                    output += BigInteger.Pow(2, i);
+                }
+            }
+
+            return output;
+        }
+
+        public override string ToString()
+        {
+            return CalculateValue().ToString();
+        }
+    }
+}
---
+++ BitArray/BitArrayTester.cs
@@ -0,0 +1,17 @@
+﻿namespace BitArray
+{
+    using System;
+
+    static class BitArrayTester
+    {
+        static void Main()
+        {
+            var num = new BitArray(100000);
+
+            num[876] = 1;
+
+            Console.WriteLine(num);
+            
+        }
+    }
+}
